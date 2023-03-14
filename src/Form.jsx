807f40cf52a3cf02b06ba5e5a0247b571f130507
john smilga <johnsmilga@gmail.com>
2023-03-14 10:19:35 -0400

    add color input

--- src/Form.jsx
+++ src/Form.jsx
@@ -13,12 +13,17 @@ const Form = ({ addColor }) => {
       <h4>color generator</h4>
       <form className='color-form' onSubmit={handleSubmit}>
         <input
+          type='color'
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <input
           type='text'
           value={color}
           onChange={(e) => setColor(e.target.value)}
           placeholder='#f15025'
         />
-        <button className='btn' type='submit'>
+        <button className='btn' type='submit' style={{ background: color }}>
           submit
         </button>
       </form>
